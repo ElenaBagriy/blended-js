630bac4c05a557e35b3e1b10595ec39516a8e562
ElenaBagriy <elena.bagriy1989@gmail.com>
2022-12-02 23:29:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,125 +42,179 @@
 // І була пані на палубі
 // Де помити мопед
 
-function palindrom(string) {
-  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+// function palindrom(string) {
+//   const normalizedString = string.replaceAll(' ', '').toLowerCase();
 
-  console.log(normalizedString);
+//   console.log(normalizedString);
 
-  const reversedString = [...normalizedString].reverse().join('');
-  console.log(reversedString);
+//   const reversedString = [...normalizedString].reverse().join('');
+//   console.log(reversedString);
 
-  console.log(reversedString === normalizedString);
-}
-palindrom('Де помити мопед');
+//   console.log(reversedString === normalizedString);
+// }
+// palindrom('Де помити мопед');
 
-const people = [
-  {
-    name: 'Alex',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Jhon',
-    know: [],
-  },
-  {
-    name: 'Eva',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Ivan',
-    know: ['Jhon', 'Eva'],
-  },
-];
-// нарцис  'Jhon'
+// const people = [
+//   {
+//     name: 'Alex',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: [],
+//   },
+//   {
+//     name: 'Eva',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Eva'],
+//   },
+// ];
+// // нарцис  'Jhon'
 
-const people1 = [
-  {
-    name: 'Alex',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Jhon',
-    know: [],
-  },
-  {
-    name: 'Eva',
-    know: [],
-  },
-  {
-    name: 'Ivan',
-    know: ['Jhon', 'Eva'],
-  },
-];
-//немає нарциса'
+// const people1 = [
+//   {
+//     name: 'Alex',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: [],
+//   },
+//   {
+//     name: 'Eva',
+//     know: [],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Eva'],
+//   },
+// ];
+// //немає нарциса'
 
-const people2 = [
-  {
-    name: 'Alex',
-    know: ['Alex', 'Eva'],
-  },
-  {
-    name: 'Jhon',
-    know: [],
-  },
-  {
-    name: 'Eva',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Ivan',
-    know: ['Jhon', 'Eva'],
-  },
-];
-//немає нарциса
+// const people2 = [
+//   {
+//     name: 'Alex',
+//     know: ['Alex', 'Eva'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: [],
+//   },
+//   {
+//     name: 'Eva',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Eva'],
+//   },
+// ];
+// //немає нарциса
 
-const people3 = [
-  {
-    name: 'Alex',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Jhon',
-    know: ['Eva'],
-  },
-  {
-    name: 'Eva',
-    know: ['Alex', 'Jhon'],
-  },
-  {
-    name: 'Ivan',
-    know: ['Jhon', 'Eva'],
-  },
-];
-//немає нарциса'
-// Нарциса знають всі, нарцис незнає нікого
+// const people3 = [
+//   {
+//     name: 'Alex',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: ['Eva'],
+//   },
+//   {
+//     name: 'Eva',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Eva'],
+//   },
+// ];
+// //немає нарциса'
+// // Нарциса знають всі, нарцис незнає нікого
 
-const isNarciss = arr => {
-  let candidat = '';
-  for (let person of arr) {
-    if (person.know.length === 0) {
-      candidat = person.name;
-    }
-  }
+// const isNarciss = arr => {
+//   let candidat = '';
+//   for (let person of arr) {
+//     if (person.know.length === 0) {
+//       candidat = person.name;
+//     }
+//   }
 
-  if (!candidat) {
-    console.log('немає нарциса');
-    return;
-  }
+//   if (!candidat) {
+//     console.log('немає нарциса');
+//     return;
+//   }
 
-  for (let person of arr) {
-    if (person.name === candidat) {
-      continue;
-    }
-    if (!person.know.includes(candidat)) {
-      console.log('немає нарциса');
-      return;
-    }
-  }
-  console.log(candidat);
+//   for (let person of arr) {
+//     if (person.name === candidat) {
+//       continue;
+//     }
+//     if (!person.know.includes(candidat)) {
+//       console.log('немає нарциса');
+//       return;
+//     }
+//   }
+//   console.log(candidat);
+// };
+
+// isNarciss(people);
+
+
+//1. Напиши скрипт для об'єкта user,
+//Послідовно:
+//1 додасть поле mood зі значенням 'happy'
+//2 замінить hobby на 'skydiving'
+//3 замінить значення premium на false
+//4 виводить вміст об'єкта users у форматі
+//ключ:значення використовуючи Object.keys() і for...of
+
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tenis",
+  premium: true,
+  
 };
 
-isNarciss(people);
+// user.mood = 'happy';
+// user['mood'] = 'happy';
+
+// const userMood = 'mood';
+
+// user[userMood] = 'happy';
+
+// console.log(user);
+
+// user.hobby = 'skydiving';
+// user['hobby'] = 'skydiving';
+
+// const userHobby = 'hobby';
+
+// user[userHobby] = 'skydiving';
+
+user.premium = false;
+user['premium'] = false;
+
+const userPremium = 'premium';
+
+user[userPremium] = false;
 
 
+const keys = Object.keys(user);
 
+for (const key of keys) {
+  console.log(`${key} - ${user[key]}`);
+}
+
+const a = {};
+
+user[a] = 'Pamaparam';
+
+console.log(user);
+
+const b = {};
+user[b] = 123214;
+console.log(user);
