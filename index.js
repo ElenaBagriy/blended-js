fcf3de05ba6dca10ebcfe5a29a3a1ee44c208f8e
Erika Tatchyn <erika.ucu@gmail.com>
2022-12-02 20:42:35 +0000

    task4

--- index.js
+++ index.js
@@ -23,21 +23,125 @@
 //Аргументів і повертає їхнє середнє значення.
 //Додати перевірку, що аргументи це числа.
 
-function caculculateAverage(...args) {
-    let total = 0;
-    let calm = 0;
-    for (let i = 0; i < args.length; i += 1) {
-        if (typeof args[i] !== 'number') {
-            continue
-        }
-        total += args[i];
-        calm += 1;
+// function caculculateAverage(...args) {
+//     let total = 0;
+//     let calm = 0;
+//     for (let i = 0; i < args.length; i += 1) {
+//         if (typeof args[i] !== 'number') {
+//             continue
+//         }
+//         total += args[i];
+//         calm += 1;
+//     }
+//     console.log((total/calm).toFixed(2));
+// }
+
+// caculculateAverage(12, 18, 32, 'testa', 'testb');
+
+const people = [
+  {
+    name: 'Alex',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Jhon',
+    know: [],
+  },
+  {
+    name: 'Eva',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Ivan',
+    know: ['Jhon', 'Eva'],
+  },
+];
+// нарцис  'Jhon'
+
+const people1 = [
+  {
+    name: 'Alex',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Jhon',
+    know: [],
+  },
+  {
+    name: 'Eva',
+    know: [],
+  },
+  {
+    name: 'Ivan',
+    know: ['Jhon', 'Eva'],
+  },
+];
+//немає нарциса'
+
+const people2 = [
+  {
+    name: 'Alex',
+    know: ['Alex', 'Eva'],
+  },
+  {
+    name: 'Jhon',
+    know: [],
+  },
+  {
+    name: 'Eva',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Ivan',
+    know: ['Jhon', 'Eva'],
+  },
+];
+//немає нарциса
+
+const people3 = [
+  {
+    name: 'Alex',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Jhon',
+    know: ['Eva'],
+  },
+  {
+    name: 'Eva',
+    know: ['Alex', 'Jhon'],
+  },
+  {
+    name: 'Ivan',
+    know: ['Jhon', 'Eva'],
+  },
+];
+//немає нарциса'
+// Нарциса знають всі, нарцис незнає нікого
+
+const isNarciss = arr => {
+  let candidat = '';
+  for (let person of arr) {
+    if (person.know.length === 0) {
+      candidat = person.name;
     }
-    console.log((total/calm).toFixed(2));
-}
+  }
 
-caculculateAverage(12, 18, 32, 'testa', 'testb');
+  if (!candidat) {
+    console.log('немає нарциса');
+    return;
+  }
 
+  for (let person of arr) {
+    if (person.name === candidat) {
+      continue;
+    }
+    if (!person.know.includes(candidat)) {
+      console.log('немає нарциса');
+      return;
+    }
+  }
+  console.log(candidat);
+};
 
-
-
+isNarciss(people);
