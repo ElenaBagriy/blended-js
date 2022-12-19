4e9f7c3a61a3246e6f7ee093806e00ce957ee598
ElenaBagriy <elena.bagriy1989@gmail.com>
2022-12-19 18:00:28 +0200

    last - dehelena

---
+++ 02-desktop-game/game.js
@@ -0,0 +1,44 @@
+
+
+//Створити невелику гру:)
+// - Спочатку на екрані користувача відображатиметься
+//яка - то форма (коло, квадрат, прямокутник)
+// - При натисканні на неї в рандомному порядку форма повинна
+//змінюватися на іншу
+// - Форма щоразу повинна з'являтися у різних місцях на сторінці
+// - Колір форми в рандомному порядку змінюється,
+
+const forms = [
+  'width: 100px; height: 100px; border-width: 1px; border-color: #000000',
+  'width: 100px; height: 100px; border-radius: 50%; border-width: 1px; border-color: #000000',
+  'width: 150px; height: 100px; border-width: 1px; border-color: #000000',
+  'width: 200px; height: 100px; border-radius: 100px / 50px;',
+  'width: 150px; height: 100px; transform: skew(20deg);',
+];
+
+const container = document.querySelector('.container');
+const formEl = document.createElement('div');
+
+onformElClick();
+
+container.append(formEl);
+
+formEl.addEventListener('click', onformElClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function randomither(max) {
+  return Math.floor(Math.random() * max);
+};
+
+function onformElClick() {
+  formEl.style.cssText = forms[randomither(forms.length)];
+  formEl.style.background = getRandomHexColor();
+  const height = 100 - (formEl.clientHeight * 100) / document.documentElement.clientHeight;
+  const width = 100 - (formEl.clientWidth * 100) / document.documentElement.clientWidth;
+  formEl.style.position = 'absolute';
+  formEl.style.top = `${randomither(height)}%`;
+  formEl.style.left = `${randomither(width)}%`;
+}
